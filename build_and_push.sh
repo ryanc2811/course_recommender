42f63bc5ff0ab6527a498aef9390bc8c7541a663
ryanc2811 <61228340+ryanc2811@users.noreply.github.com>
2022-12-17 03:04:43 +0000

    test 8

--- build_and_push.sh
+++ build_and_push.sh
@@ -16,38 +16,7 @@ fi
 chmod +x src/train
 chmod +x src/serve
 
-# Get the account number associated with the current IAM credentials
-account=$(aws sts get-caller-identity --query Account --output text)
-
-if [ $? -ne 0 ]
-then
-    exit 255
-fi
-
-
-# Get the region defined in the current configuration (default to us-west-2 if none defined)
-region=$(aws configure get region)
-region=${region:-eu-west-2}
-
-
-fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:latest"
-
-# If the repository doesn't exist in ECR, create it.
-
-aws ecr describe-repositories --repository-names "${image}" > /dev/null 2>&1
-
-if [ $? -ne 0 ]
-then
-    aws ecr create-repository --repository-name "${image}" > /dev/null
-fi
-
-# Get the login command from ECR and execute it directly
-aws ecr get-login-password --region "${region}" | docker login --username AWS --password-stdin "${account}".dkr.ecr."${region}".amazonaws.com
-
-# Build the docker image locally with the image name and then push it to ECR
-# with the full name.
 
 docker build  -t ${image} .
-docker tag ${image} ${fullname}
 
-docker push ${fullname}
+docker run -p 5000:80 --name recommender ${image} 
