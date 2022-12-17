8e6c639f9b17ca2e1bf470e9746863306144a4f0
ryanc2811 <61228340+ryanc2811@users.noreply.github.com>
2022-12-17 08:30:51 +0000

    test

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, Response, request
 from collections import defaultdict
 import pandas as pd
 from sklearn.cluster import KMeans
@@ -121,25 +121,26 @@ def predict(user_id, predictions):
 
 predictions,category_watch_time_df = generate_user_model()
 
-@app.route('/predict-user/<string:user_id>')
-def get_user_recommended_categories(user_id):
+@app.route('/predict', methods=['POST'])
+def get_user_recommended_categories():
+    content = request.json
+    user_id = content['UserID']
+
     # Check that user exists:
     try:
         users,courses=load_data()
         users.loc[user_id]
     except:
-        return jsonify({"error": "The user does not exist"})
-    user_predicted_categories = predict(user_id, predictions)
-    return jsonify({"predicted_categories": user_predicted_categories})
+        return Response(response="User Does not exist", status=200, mimetype="application/json")
+    response= predict(user_id, predictions)
+    return Response(response=response, status=200, mimetype="application/json")
 
 
-@app.route('/ping')
+@app.route('/ping', methods=['GET'])
 def ping():
-    """Determine if the container is working and healthy. In this sample container, we declare
-    it healthy if we can load the model successfully."""
-    health = generate_user_model is not None
+    health = predictions is not None
     status = 200 if health else 404
-    return jsonify({"status":status})
+    return Response(response='\n', status=status, mimetype='application/json')
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0',port=80)
