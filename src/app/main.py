4afaed5eb31cc8c8e42afc24a54c36788cf8c98d
ryanc2811 <61228340+ryanc2811@users.noreply.github.com>
2022-12-17 12:11:41 +0000

    test

--- src/app/main.py
+++ src/app/main.py
@@ -99,8 +99,8 @@ def predict(user_id, predictions):
     user_cluster_df = user_cluster_df[user_cluster_df.columns[user_cluster_df.columns.isin(['UserID', 'assigned_cluster'])]]
 
     progress_df,courses_df=load_data()
-    cluster = progress_df.loc[progress_df['UserID'] == user_id].values[0][0]
-    user_cluster_df= progress_df.loc[progress_df['assigned_cluster'] == cluster]
+    cluster = user_cluster_df.loc[user_cluster_df['UserID'] == user_id].assigned_cluster.values[0]
+    user_cluster_df= user_cluster_df.loc[user_cluster_df['assigned_cluster'] == cluster]
 
     cluster_users_df = progress_df[progress_df['UserID'].isin(user_cluster_df['UserID'])]
     #convert progress percentage string to numeric data
