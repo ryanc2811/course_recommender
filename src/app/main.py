542c13ecedefb60f1b66723d1d12000fcf0f0c21
ryanc2811 <61228340+ryanc2811@users.noreply.github.com>
2022-12-17 11:10:16 +0000

    test

--- src/app/main.py
+++ src/app/main.py
@@ -122,7 +122,7 @@ def predict(user_id, predictions):
 predictions,category_watch_time_df = generate_user_model()
 
 
-@app.route('/predict/<string:user_id>',methods=["POST"])
+@app.route('/predict/<int:user_id>',methods=["POST"])
 def prefict(user_id):
     if request.method=="POST":
  
